0e7e7564400ac93f7e749b4909a2d05fec539364
Dasom <73115272+racheljeong@users.noreply.github.com>
2021-08-06 15:20:57 +0900

    0806

---
+++ 두개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    var answer = [];
+   
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = i+1; j < numbers.length; j++){
+           if(!answer.includes(numbers[i]+numbers[j]))
+             answer.push(numbers[i]+numbers[j]);
+        }
+    }
+    
+    answer = answer.sort(function(a,b){
+      return (a - b);
+    });
+   
+    return answer;
+}
